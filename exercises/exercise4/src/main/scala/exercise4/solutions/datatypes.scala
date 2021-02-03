638d6479b0ec87ecd70f2ee75807c7bf6d7762d1
Álvaro Castellanos López <alvaro.castellanos@47deg.com>
2021-02-03 11:23:15 +0100

    exercise 4

---
+++ exercises/exercise4/src/main/scala/exercise4/solutions/datatypes.scala
@@ -0,0 +1,33 @@
+package exercise4.solutions
+
+import java.util.Random
+import scala.util.Try
+
+object Exercise4 {
+  val random = new Random
+
+  val maybeString: String =
+    if (random.nextBoolean) {
+      "it's not null"
+    } else {
+      null
+    }
+
+  /**
+   * reimplement this using Option
+   */
+  def useOption: Option[String] = Option(maybeString)
+
+  val stringOrThrow: String =
+    if (random.nextBoolean) {
+      "it's not null"
+    } else {
+      throw new Exception("oh oh")
+    }
+
+  /**
+   * reimplement this using try
+   */
+  def useTry: Try[String] = Try(stringOrThrow)
+
+}
