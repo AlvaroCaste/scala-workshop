2817ba97943f75b5596171cd82e924ab9d886ec2
Álvaro Castellanos López <alvaro.castellanos@47deg.com>
2021-02-03 10:57:34 +0100

    exercise 3.1 resolved

--- exercises/exercise3/src/main/scala/exercise3/tree.scala
+++ exercises/exercise3/src/main/scala/exercise3/tree.scala
@@ -9,6 +9,12 @@ object Tree {
   /**
    * implement the fold function
    */
-  def fold[A, B](tree: Tree[A])(onEmpty: B, onNode: (B, A, B) => B): B = ???
-
+  def fold[A, B](tree: Tree[A])(onEmpty: B, onNode: (B, A, B) => B): B = tree match {
+    case Empty() => onEmpty
+    case Node(left, a, right) => onNode(
+      fold(left)(onEmpty, onNode),
+      a,
+      fold(right)(onEmpty, onNode)
+    )
+  }
 }
