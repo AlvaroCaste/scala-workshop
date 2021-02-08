7b221b57bf201f826b6ca05ffb6c4a55353a5bfe
Álvaro Castellanos López <alvaro.castellanos@47deg.com>
2021-02-08 13:19:23 +0100

    exercise 5.3 description

--- exercises/exercise5/src/main/scala/exercise5/typeclasses.scala
+++ exercises/exercise5/src/main/scala/exercise5/typeclasses.scala
@@ -1,6 +1,6 @@
 package exercise5
 
-import cats.{Eq, Eval, Foldable, Functor}
+import cats.{Eq, Eval, Foldable, Functor, Monad}
 import exercise3.{Empty, Node, Tree}
 
 object typeclasses {
@@ -30,5 +30,25 @@ object typeclasses {
       case Empty() => lb
       case Node(l, a, r) => foldRight(r, foldRight(l, f(a, lb))(f))(f)
     }
+  }
+
+  /**
+   * create a Monad instance for Maybe
+   */
+  sealed trait Maybe[A]
+  case class Nothing[A]() extends Maybe[A]
+  case class Just[A](a: A) extends Maybe[A]
+
+  implicit val maybeMonad: Monad[Maybe] = new Monad[Maybe] {
+    override def pure[A](x: A): Maybe[A] = ???
+
+    override def flatMap[A, B](fa: Maybe[A])(f: A => Maybe[B]): Maybe[B] = ???
+
+    /**
+     * This method was not originally part of the Monad typeclass,
+     * although is added for performance
+     */
+    override def tailRecM[A, B](a: A)(f: A => Maybe[Either[A, B]]): Maybe[B] = ???
+
   }
 }
