eb8a4e260f6ca64fa2124e209a65ed8781ae09c2
Álvaro Castellanos López <alvaro.castellanos@47deg.com>
2021-02-09 15:25:40 +0100

    exercise case class desugaring resolved

--- exercises/exercise2/src/main/scala/exercise2/LabelledPoint.scala
+++ exercises/exercise2/src/main/scala/exercise2/LabelledPoint.scala
@@ -1,3 +1,37 @@
 package exercise2
 
-class LabelledPoint(label: String, x: Int, y: Int)
+class LabelledPoint(val label: String, val x: Int, val y: Int) extends Serializable with Product {
+
+  override def toString: String = s"LabelledPoint($label,$x,$y)"
+
+  override def equals(obj: Any): Boolean = obj match {
+    case lp: LabelledPoint => label == lp.label && x == lp.x && y == lp.y
+    case _ => throw new ClassCastException
+  }
+
+  override def hashCode(): Int = 31 * label.hashCode * x.hashCode * y.hashCode
+
+  override def productArity: Int = 3
+
+  override def productElement(n: Int): Any = n match {
+    case 0 => label
+    case 1 => x
+    case 2 => y
+    case _ => throw new IndexOutOfBoundsException(n)
+  }
+
+  override def productPrefix: String = "LabelledPoint"
+
+  override def canEqual(that: Any): Boolean = that match {
+    case LabelledPoint => true
+    case _ => false
+  }
+
+  def copy(label: String = label, x: Int = x, y: Int = y): LabelledPoint = LabelledPoint(label, x, y)
+}
+
+object LabelledPoint {
+  def apply(label: String, x: Int, y: Int): LabelledPoint = new LabelledPoint(label, x, y)
+
+  def unapply(arg: LabelledPoint): Option[(String, Int, Int)] = Option(arg.label, arg.x, arg.y)
+}
--- exercises/exercise2/src/test/scala/exercise2/LabelledPointTest.scala
+++ exercises/exercise2/src/test/scala/exercise2/LabelledPointTest.scala
@@ -14,77 +14,68 @@ class LabelledPointTest extends AnyFlatSpec with Matchers with ScalaCheckDrivenP
 
 
   "A 'case' class like LabelledPoint" should "have getters" in {
-    fail("Uncomment and fix")
-    //    val value = new LabelledPoint("label", 1, 2)
-    //
-    //    value.label shouldBe "label"
-    //    value.x shouldBe 1
-    //    value.y shouldBe 2
+    val value = new LabelledPoint("label", 1, 2)
+
+    value.label shouldBe "label"
+    value.x shouldBe 1
+    value.y shouldBe 2
   }
 
   it should "offer a factory method" in {
-    fail("Uncomment and fix")
-    //    val value = LabelledPoint("label", 1, 2)
-    //
-    //    value.label shouldBe "label"
-    //    value.x shouldBe 1
-    //    value.y shouldBe 2
+    val value = LabelledPoint("label", 1, 2)
+
+    value.label shouldBe "label"
+    value.x shouldBe 1
+    value.y shouldBe 2
   }
 
   it should "have a nice to string method" in {
-    fail("Uncomment and fix")
-    //    LabelledPoint("label", 1, 2).toString shouldBe "LabelledPoint(label,1,2)"
+    LabelledPoint("label", 1, 2).toString shouldBe "LabelledPoint(label,1,2)"
   }
 
   it should "implement extensional equality" in {
-    fail("Uncomment and fix")
-//    forAll { (label: String, x: Int, y: Int) =>
-      //      LabelledPoint(label, x, y) shouldBe LabelledPoint(label, x, y)
-      //      LabelledPoint(label, x, y) should not be LabelledPoint(label + "!", x, y)
-      //      LabelledPoint(label, x, y) should not be LabelledPoint(label, x + 1, y)
-      //      LabelledPoint(label, x, y) should not be LabelledPoint(label, x, y - 2)
-//    }
+    forAll { (label: String, x: Int, y: Int) =>
+      LabelledPoint(label, x, y) shouldBe LabelledPoint(label, x, y)
+      LabelledPoint(label, x, y) should not be LabelledPoint(label + "!", x, y)
+      LabelledPoint(label, x, y) should not be LabelledPoint(label, x + 1, y)
+      LabelledPoint(label, x, y) should not be LabelledPoint(label, x, y - 2)
+    }
   }
 
   it should "implement extensional hash code" in {
-    fail("Uncomment and fix")
-//    forAll { (value: LabelledPoint) =>
-      //      val pointWithSameData = new LabelledPoint(value.label, value.x, value.y)
-      //      value.hashCode() shouldBe pointWithSameData.hashCode()
-//    }
+    forAll { (value: LabelledPoint) =>
+      val pointWithSameData = new LabelledPoint(value.label, value.x, value.y)
+      value.hashCode() shouldBe pointWithSameData.hashCode()
+    }
   }
 
   it should "be serializable" in {
-    fail("Uncomment and fix")
-    //    LabelledPoint("label", 1, 2) shouldBe a [Serializable]
+    LabelledPoint("label", 1, 2) shouldBe a [Serializable]
   }
 
   it should "be a product type" in {
-    fail("Uncomment and fix")
-    //    val value = LabelledPoint("name", 5, 7)
-    //
-    //    value.productPrefix shouldBe "LabelledPoint"
-    //    value.productArity shouldBe 3
-    //    value.productElement(0) shouldBe "name"
-    //    value.productElement(1) shouldBe 5
-    //    value.productElement(2) shouldBe 7
-    //    value.productIterator.toList shouldBe List("name", 5, 7)
+    val value = LabelledPoint("name", 5, 7)
+
+    value.productPrefix shouldBe "LabelledPoint"
+    value.productArity shouldBe 3
+    value.productElement(0) shouldBe "name"
+    value.productElement(1) shouldBe 5
+    value.productElement(2) shouldBe 7
+    value.productIterator.toList shouldBe List("name", 5, 7)
   }
 
   it should "have a copy method" in {
-    fail("Uncomment and fix")
-    //    val value = LabelledPoint("name", 5, 7)
-    //    value.copy(label = "other") shouldBe LabelledPoint("other", 5, 7)
-    //    value.copy(x = 1, y = 2) shouldBe LabelledPoint("name", 1, 2)
+    val value = LabelledPoint("name", 5, 7)
+    value.copy(label = "other") shouldBe LabelledPoint("other", 5, 7)
+    value.copy(x = 1, y = 2) shouldBe LabelledPoint("name", 1, 2)
   }
 
   it should "have an extractor" in {
-    fail("Uncomment and fix")
-    //    LabelledPoint.unapply(LabelledPoint("name", 5, 7)) shouldBe Some("name", 5, 7)
-    //
-    //    val values = Seq.tabulate(10) { index => LabelledPoint(s"point$index", index % 2, index) }
-    //    values.collect {
-    //      case LabelledPoint(name, 0, index) if index < 5 => name
-    //    } shouldBe Seq("point0", "point2", "point4")
+    LabelledPoint.unapply(LabelledPoint("name", 5, 7)) shouldBe Some("name", 5, 7)
+
+    val values = Seq.tabulate(10) { index => LabelledPoint(s"point$index", index % 2, index) }
+    values.collect {
+      case LabelledPoint(name, 0, index) if index < 5 => name
+    } shouldBe Seq("point0", "point2", "point4")
   }
 }
